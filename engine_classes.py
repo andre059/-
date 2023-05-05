669ef5e8a9f96b57a27343e750c733f8e32bae22
andre059 <andreyperm159@mail.ru>
2023-05-05 17:59:04 +0500

    2.2.2

--- engine_classes.py
+++ engine_classes.py
@@ -21,69 +21,42 @@ class HH(Engine):
     def __init__(self, secret_key):
         self.secret_key = secret_key
 
-    def get_request(self, page: int = 10):
-        params = {
-            'text': 'NAME:Аналитик',  # Текст фильтра. В имени должно быть слово "Аналитик"
-            'area': 1,  # Поиск ощуществляется по вакансиям города Москва
-            'page': page,  # Индекс страницы поиска на HH
-            'per_page': 100  # Кол-во вакансий на 1 странице
-        }
+    def get_request(self, page: int = 11):
+        params = {'per_page': 100, 'only_with_salary': True}
+
+        items: list[dict] = []
 
         for page in range(0, page):
-            # Преобразуем текст ответа запроса в справочник Python
-            jsObj = json.loads(self.get_request(page))
-            # Создаем новый документ, записываем в него ответ запроса, после закрываем
-            with open("data_file.json", "w", encoding="UTF-8") as file:
-                json.dump(jsObj, file)
-            # Проверка на последнюю страницу, если вакансий меньше 1000
-            if (jsObj['pages'] - page) <= 1000:
-                break
+            params['page'] = page
+            response = requests.get('https://api.hh.ru/vacancies', params)
+            # response.raise_for_status()
 
-        req = requests.get('https://api.hh.ru/vacancies', params)  # Посылаем запрос к API
-        data = req.content.decode()  # Декодируем его ответ, чтобы Кириллица отображалась корректно
-        req.close()
-        return data
+            data = response.json()
+            items.extend(data['items'])
+
+            with open("data_file.json", "w", encoding="UTF-8") as file:
+                json.dump(items, file)
+
+        return items
 
     def get_vacancies(self):
-        api_url = 'https://api.hh.ru/vacancies'
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                                 'Chrome/58.0.3029.110 Safari/537.3'}
-        params = {'text': 'Python разработчик', 'per_page': 100}  # параметры запроса (название вакансии)
+        count = 0
 
-        vacancies = []
-        page = 0
+        with open("list_of_vacancies.json", "r", encoding="UTF-8") as file:
+            data = json.load(file)
 
-        while len(vacancies) < 1000:
-            count = 1
-            page += 1
-            params['page'] = page
-            response = requests.get(api_url, headers=headers, params=params)  # выполнение запроса
-
-            if response.ok:  # проверяем успешность запроса
-                data = response.json()  # получаем список вакансий в формате JSON
-                if len(data['items']) == 0:
-                    break
+        for vacancy in data:
+            if vacancy is not None:
+                if vacancy['currency'] == 'RUR':  # выводим только зарплату в рублях
+                    count += 1
+                    # выводим наименование вакансии и зарплату
+                    print(count, ')', vacancy['name'], vacancy['from'], '-', vacancy['to'], vacancy['currency'])
                 else:
-                    vacancies += data['items']
-            else:
-                print('Ошибка выполнения запроса 1')
-                break
+                    count += 1
+                    print(count, ')', vacancy['name'], 'Зарплата не указана')
 
-            for vacancy in vacancies:
-                salary = vacancy['salary']  # получаем информацию о зарплате
-                if salary is not None:
-                    if salary['currency'] == 'RUR':  # выводим только зарплату в рублях
-                        # выводим наименование вакансии и зарплату
-                        print(count, ')', vacancy['name'], salary['from'], '-', salary['to'], salary['currency'])
-                        count += 1
-                    else:
-                        print(count, ')', vacancy['name'], 'Зарплата не указана')
-                        count += 1
-
-                    with open("list_of_vacancies.json", "w", encoding="UTF-8") as file:
-                        json.dump(vacancy, file)
-            else:
-                print('Ошибка выполнения запроса 2')
+            with open("list_of_vacancies.json", "w", encoding="UTF-8") as f:
+                json.dump(vacancy, f)
 
 
 class SuperJob(Engine):
